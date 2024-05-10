ebea88b96de80c950bc9e697a408244c8252ffac
Rohan Tayal <109153434+rohantayal@users.noreply.github.com>
2024-05-10 19:16:35 +0530

    Added a smooth dropdown

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-    const faqs= document.querySelectorAll(".faq");
+    const faqs = document.querySelectorAll(".faq");
     console.log(faqs);
 
     faqs.forEach(faq => {
-        faq.addEventListener('click',()=>{
+        faq.addEventListener('click', () => {
             const answer= faq.querySelector(".answer");
-            const arrow=faq.querySelector(".arrow");
-    
-            if(answer.classList.contains("active")){
+            const arrow= faq.querySelector(".arrow");
+
+            faqs.forEach(faq2 => {
+                const answer2 = faq2.querySelector(".answer");
+                const arrow2 = faq2.querySelector(".arrow");
+                if(faq2 !== faq){
+                    if (answer2.classList.contains("active")) {
+                        answer2.classList.remove("active");
+                        arrow2.alt = "Expand";
+                        arrow2.classList.remove("rotate");
+                    }
+                }
+            })
+
+
+            if (answer.classList.contains("active")) {
                 answer.classList.remove("active");
-                arrow.alt="Expand";
+                arrow.alt = "Expand";
                 arrow.classList.remove("rotate");
-            }else{
+            } else {
                 answer.classList.add("active");
-                arrow.alt="Collapse";
+                arrow.alt = "Collapse";
                 arrow.classList.add("rotate");
             }
-            
+
         })
 
-        
+
     });
 })
